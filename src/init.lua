d5eb107d42b8841d4dadb90a22c5f183d3172674
Raine <155107924+raineyraine@users.noreply.github.com>
2024-09-19 16:04:08 -0400

    Update init.lua

--- src/init.lua
+++ src/init.lua
@@ -1,20 +1,65 @@
-local RaineParticle = { }
-RaineParticle.__index = RaineParticle
+local RunService = game:GetService("RunService")
+local Signal = require(script.Signal)
 
-function RaineParticle.new()
-  local self = { }
-  self.__dead = false
+local Lifetime = { }
+Lifetime.__index = Lifetime
 
-  return setmetatable(self, RaineParticle)
+function Lifetime.new()
+	local self = { }
+	self.__destroyed = false
+	self.Dead = true
+	self.Lifespan = 1
+	self.Age = 1
+	
+	self.Revived = Signal.new()
+	self.Died = Signal.new()
+	
+	return setmetatable(self, Lifetime)
 end
 
-function RaineParticle:Destroy()
-  if not self.__dead then
-    error("Particle is already dead!")
-    return
-  end
-  
-  self.__dead = true
+function Lifetime:Revive()
+	self.Age = 0
+	self.Dead = false
+	
+	if self.__RunServiceConnection then
+		self.__RunServiceConnection:Disconnect()
+	end
+	
+	self.__RunServiceConnection = RunService.Heartbeat:Connect(function(deltaTime: number)
+		self.Age += deltaTime
+		if self.Age >= self.Lifespan then
+			self.Age = self.Lifespan
+			self.Dead = true
+			self.Died:Fire()
+			self.__RunServiceConnection:Disconnect()
+		end
+	end)
+
+	self.Revived:Fire()
 end
 
-return RaineParticle
+function Lifetime:Kill()
+	self.Age = self.Lifespan
+	self.Dead = true
+	self.Died:Fire()
+	if self.__RunServiceConnection then
+		self.__RunServiceConnection:Disconnect()
+	end
+end
+
+function Lifetime:Destroy()
+	if self.__destroyed then
+		error("Cannot destroy a destroyed Lifetime!")
+		return
+	end
+	
+	self.Revived:Destroy()
+	self.Died:Destroy()
+	if self.__RunServiceConnection then
+		self.__RunServiceConnection:Disconnect()
+	end
+	
+	self.__destroyed = true
+end
+
+return Lifetime
